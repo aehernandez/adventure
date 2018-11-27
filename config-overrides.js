af79800fb893f915154700cb87166e7a5db042ed
Alain Hernandez <alainehernandez@gmail.com>
2018-11-27 02:15:26 -0500

    babel overrides

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  injectBabelPlugin('babel-plugin-styled-components', config);
+  return config;
+}
