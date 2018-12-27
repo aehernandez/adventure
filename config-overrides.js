07df4e36bdafd1aa3c39ac43ff6262c28b7cea1f
Alain Hernandez <alainehernandez@gmail.com>
2018-12-26 21:32:45 -0800

    allow decorators

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
+  injectBabelPlugin(['@babel/plugin-proposal-decorators', { legacy: true }], config);
   injectBabelPlugin('babel-plugin-styled-components', config);
   return config;
 }
